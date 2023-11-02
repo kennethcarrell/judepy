9daaa1e4459a3c71f044900d0bb9e83397f2c3cc
Kenneth Carrell <kennethcarrell@gmail.com>
2023-11-02 16:01:08 -0500

    several updates

--- distortion.py
+++ distortion.py
@@ -1,74 +1,298 @@
+import re
+import statistics
+
 ####
 #### a class to read in distortion information from Astrometrica ouput
 ####
 class Distortion:
-    ## give variables some initial value
-    N_stars = 0
-    x_eqn = y_eqn = ''
-    x_const = x_xp = x_yp = x_xp2 = x_xpyp = x_yp2 = x_xp3 = x_xp2yp = x_xpyp2 = x_yp3 = 0.
-    y_const = y_xp = y_yp = y_xp2 = y_xpyp = y_yp2 = y_xp3 = y_xp2yp = y_xpyp2 = y_yp3 = 0.
+    ## define variables
     _verbose_ = False
 
+    N_fields = 0
+    x_eqn = ''
+    y_eqn = ''
+    N_stars = []
+
+    x_const = 0.
+    x_const_err = 0.
+    x_const_arr = []
+    x_xp = 0.
+    x_xp_err = 0.
+    x_xp_arr = []
+    x_yp = 0.
+    x_yp_err = 0.
+    x_yp_arr = []
+    x_xp2 = 0.
+    x_xp2_err = 0.
+    x_xp2_arr = []
+    x_xpyp = 0.
+    x_xpyp_err = 0.
+    x_xpyp_arr = []
+    x_yp2 = 0.
+    x_yp2_err = 0.
+    x_yp2_arr = []
+    x_xp3 = 0.
+    x_xp3_err = 0.
+    x_xp3_arr = []
+    x_xp2yp = 0.
+    x_xp2yp_err = 0.
+    x_xp2yp_arr = []
+    x_xpyp2 = 0.
+    x_xpyp2_err = 0.
+    x_xpyp2_arr = []
+    x_yp3 = 0.
+    x_yp3_err = 0.
+    x_yp3_arr = []
+
+    y_const = 0.
+    y_const_err = 0.
+    y_const_arr = []
+    y_xp = 0.
+    y_xp_err = 0.
+    y_xp_arr = []
+    y_yp = 0.
+    y_yp_err = 0.
+    y_yp_arr = []
+    y_xp2 = 0.
+    y_xp2_err = 0.
+    y_xp2_arr = []
+    y_xpyp = 0.
+    y_xpyp_err = 0.
+    y_xpyp_arr = []
+    y_yp2 = 0.
+    y_yp2_err = 0.
+    y_yp2_arr = []
+    y_xp3 = 0.
+    y_xp3_err = 0.
+    y_xp3_arr = []
+    y_xp2yp = 0.
+    y_xp2yp_err = 0.
+    y_xp2yp_arr = []
+    y_xpyp2 = 0.
+    y_xpyp2_err = 0.
+    y_xpyp2_arr = []
+    y_yp3 = 0.
+    y_yp3_err = 0.
+    y_yp3_arr = []
+
     ## class constructor
-    def __init__(self,fname='',vb=False):
+    def __init__(self,fname='',vb=False,reset=True):
         self._verbose_ = vb
         if(fname != ''):
-            self.readAstrometrica(fname)
+            self.readAstrometrica(fname,reset)
+
+    ## give variables some initial value
+    def clearVars(self):
+        self.N_fields = 0
+        self.x_eqn = ''
+        self.y_eqn = ''
+        self.N_stars = []
+
+        self.x_const = 0.
+        self.x_const_err = 0.
+        self.x_const_arr = []
+        self.x_xp = 0.
+        self.x_xp_err = 0.
+        self.x_xp_arr = []
+        self.x_yp = 0.
+        self.x_yp_err = 0.
+        self.x_yp_arr = []
+        self.x_xp2 = 0.
+        self.x_xp2_err = 0.
+        self.x_xp2_arr = []
+        self.x_xpyp = 0.
+        self.x_xpyp_err = 0.
+        self.x_xpyp_arr = []
+        self.x_yp2 = 0.
+        self.x_yp2_err = 0.
+        self.x_yp2_arr = []
+        self.x_xp3 = 0.
+        self.x_xp3_err = 0.
+        self.x_xp3_arr = []
+        self.x_xp2yp = 0.
+        self.x_xp2yp_err = 0.
+        self.x_xp2yp_arr = []
+        self.x_xpyp2 = 0.
+        self.x_xpyp2_err = 0.
+        self.x_xpyp2_arr = []
+        self.x_yp3 = 0.
+        self.x_yp3_err = 0.
+        self.x_yp3_arr = []
+
+        self.y_const = 0.
+        self.y_const_err = 0.
+        self.y_const_arr = []
+        self.y_xp = 0.
+        self.y_xp_err = 0.
+        self.y_xp_arr = []
+        self.y_yp = 0.
+        self.y_yp_err = 0.
+        self.y_yp_arr = []
+        self.y_xp2 = 0.
+        self.y_xp2_err = 0.
+        self.y_xp2_arr = []
+        self.y_xpyp = 0.
+        self.y_xpyp_err = 0.
+        self.y_xpyp_arr = []
+        self.y_yp2 = 0.
+        self.y_yp2_err = 0.
+        self.y_yp2_arr = []
+        self.y_xp3 = 0.
+        self.y_xp3_err = 0.
+        self.y_xp3_arr = []
+        self.y_xp2yp = 0.
+        self.y_xp2yp_err = 0.
+        self.y_xp2yp_arr = []
+        self.y_xpyp2 = 0.
+        self.y_xpyp2_err = 0.
+        self.y_xpyp2_arr = []
+        self.y_yp3 = 0.
+        self.y_yp3_err = 0.
+        self.y_yp3_arr = []
+
+    ## figure out how many fields are in the file
+    def getCount(self,fname,reset=True):
+        ## set counter
+        self.N_fields = 0
+        ## encoding is not 'utf-8' for some reason
+        with open(fname,'r',encoding='iso-8859-1') as f:
+            ## read in everything
+            buff = f.read()
+            ## count how many 'Start's there are
+            self.N_fields = len(re.findall("Start",buff))
+
+    ## separate out values
+    def parseEqn(self,eqn,isxy):
+        const = float(eqn.split()[2])
+        xp = float(eqn.split()[3].split('*')[0])
+        yp = float(eqn.split()[4].split('*')[0])
+        xp2 = float(eqn.split()[5].split('*')[0])
+        xpyp = float(eqn.split()[6].split('*')[0])
+        yp2 = float(eqn.split()[7].split('*')[0])
+        xp3 = float(eqn.split()[8].split('*')[0])
+        xp2yp = float(eqn.split()[9].split('*')[0])
+        xpyp2 = float(eqn.split()[10].split('*')[0])
+        yp3 = float(eqn.split()[11].split('*')[0])
+        if(isxy == 'x'):
+            self.x_const_arr.append(const)
+            self.x_xp_arr.append(xp)
+            self.x_yp_arr.append(yp)
+            self.x_xp2_arr.append(xp2)
+            self.x_xpyp_arr.append(xpyp)
+            self.x_yp2_arr.append(yp2)
+            self.x_xp3_arr.append(xp3)
+            self.x_xp2yp_arr.append(xp2yp)
+            self.x_xpyp2_arr.append(xpyp2)
+            self.x_yp3_arr.append(yp3)
+            return 1
+        elif(isxy == 'y'):
+            self.y_const_arr.append(const)
+            self.y_xp_arr.append(xp)
+            self.y_yp_arr.append(yp)
+            self.y_xp2_arr.append(xp2)
+            self.y_xpyp_arr.append(xpyp)
+            self.y_yp2_arr.append(yp2)
+            self.y_xp3_arr.append(xp3)
+            self.y_xp2yp_arr.append(xp2yp)
+            self.y_xpyp2_arr.append(xpyp2)
+            self.y_yp3_arr.append(yp3)
+            return 2
+        else:
+            return 0
+
 
     ## read in Astrometrica output
-    def readAstrometrica(self,fname):
+    def readAstrometrica(self,fname,reset=True):
+        if(reset):
+            self.clearVars()
+        self.getCount(fname,reset)
         ## encoding is not 'utf-8' for some reason
         with open(fname,'r',encoding='iso-8859-1') as f:
             ## read the file
             all_lines = f.readlines()
-            ## grab the number of stars used
-            self.N_stars = int(all_lines[19].split()[0])
 
-            ## information for the X solution
-            self.x_eqn = all_lines[20]+all_lines[21]+all_lines[22]
-            ## print the equation if we want to
-            if(self._verbose_):
-                print(self.x_eqn)
-            self.x_const = float(self.x_eqn.split()[2])
-            self.x_xp = float(self.x_eqn.split()[3].split('*')[0])
-            self.x_yp = float(self.x_eqn.split()[4].split('*')[0])
-            self.x_xp2 = float(self.x_eqn.split()[5].split('*')[0])
-            self.x_xpyp = float(self.x_eqn.split()[6].split('*')[0])
-            self.x_yp2 = float(self.x_eqn.split()[7].split('*')[0])
-            self.x_xp3 = float(self.x_eqn.split()[8].split('*')[0])
-            self.x_xp2yp = float(self.x_eqn.split()[9].split('*')[0])
-            self.x_xpyp2 = float(self.x_eqn.split()[10].split('*')[0])
-            self.x_yp3 = float(self.x_eqn.split()[11].split('*')[0])
-            ## print coefficients if we want to
-            if(self._verbose_):
-                print(self.x_const,self.x_xp,self.x_yp,self.x_xp2,self.x_xpyp,self.x_yp2,self.x_xp3,self.x_xp2yp,self.x_xpyp2,self.x_yp3)
+            ## step through each line in the file
+            for i in range(len(all_lines)):
+                if("Start" in all_lines[i]):
+                    offset = 0
+                    ## find offset
+                    for j in range(16,22):
+                        if "Astrometry" in all_lines[i+j]:
+                            offset = j+1
+                    ## grab the number of stars used
+                    self.N_stars.append(int(all_lines[i+offset].split()[0]))
 
-            ## information for the Y solution
-            self.y_eqn = all_lines[23]+all_lines[24]+all_lines[25]
-            ## print the equation if we want to
-            if(self._verbose_):
-                print(self.y_eqn)
-            self.y_const = float(self.y_eqn.split()[2])
-            self.y_xp = float(self.y_eqn.split()[3].split('*')[0])
-            self.y_yp = float(self.y_eqn.split()[4].split('*')[0])
-            self.y_xp2 = float(self.y_eqn.split()[5].split('*')[0])
-            self.y_xpyp = float(self.y_eqn.split()[6].split('*')[0])
-            self.y_yp2 = float(self.y_eqn.split()[7].split('*')[0])
-            self.y_xp3 = float(self.y_eqn.split()[8].split('*')[0])
-            self.y_xp2yp = float(self.y_eqn.split()[9].split('*')[0])
-            self.y_xpyp2 = float(self.y_eqn.split()[10].split('*')[0])
-            self.y_yp3 = float(self.y_eqn.split()[11].split('*')[0])
-            ## print coefficients if we want to
-            if(self._verbose_):
-                print(self.y_const,self.y_xp,self.y_yp,self.y_xp2,self.y_xpyp,self.y_yp2,self.y_xp3,self.y_xp2yp,self.y_xpyp2,self.y_yp3)
+                    ## information for the X solution
+                    self.x_eqn = all_lines[i+offset+1]+all_lines[i+offset+2]+all_lines[i+offset+3]
+                    ## print the equation if we want to
+                    if(self._verbose_):
+                        print(self.x_eqn)
+                    ## store the values from the equation
+                    self.parseEqn(self.x_eqn,'x')
+                    ## print coefficients if we want to
+                    if(self._verbose_):
+                        print(self.x_const,self.x_xp,self.x_yp,self.x_xp2,self.x_xpyp,self.x_yp2,self.x_xp3,self.x_xp2yp,self.x_xpyp2,self.x_yp3)
 
-####
-#### example usage:
-####
-#### f1 = Distortion('Bruns HIP 100453 ED127W.log')
-#### f2 = Distortion('Bruns HIP 102488 RC8.log')
-#### print(f1.N_stars,f1.x_const,f1.y_const)
-#### print(f2.N_stars,f2.x_const,f2.y_const)
-#### f3 = Distortion()
-#### f3.readAstrometrica('Bruns HIP 100453 ED127W.log')
-#### print(f3.N_stars,f3.x_const,f3.y_const)
+                    ## information for the Y solution
+                    self.y_eqn = all_lines[i+offset+4]+all_lines[i+offset+5]+all_lines[i+offset+6]
+                    ## print the equation if we want to
+                    if(self._verbose_):
+                        print(self.y_eqn)
+                    ## store the values from the equation
+                    self.parseEqn(self.y_eqn,'y')
+                    ## print coefficients if we want to
+                    if(self._verbose_):
+                        print(self.y_const,self.y_xp,self.y_yp,self.y_xp2,self.y_xpyp,self.y_yp2,self.y_xp3,self.y_xp2yp,self.y_xpyp2,self.y_yp3)
+
+        ## calculate the average and stdev values
+        self.x_const = statistics.mean(self.x_const_arr)
+        self.x_xp = statistics.mean(self.x_xp_arr)
+        self.x_yp = statistics.mean(self.x_yp_arr)
+        self.x_xp2 = statistics.mean(self.x_xp2_arr)
+        self.x_xpyp = statistics.mean(self.x_xpyp_arr)
+        self.x_yp2 = statistics.mean(self.x_yp2_arr)
+        self.x_xp3 = statistics.mean(self.x_xp3_arr)
+        self.x_xp2yp = statistics.mean(self.x_xp2yp_arr)
+        self.x_xpyp2 = statistics.mean(self.x_xpyp2_arr)
+        self.x_yp3 = statistics.mean(self.x_yp3_arr)
+
+        self.y_const = statistics.mean(self.y_const_arr)
+        self.y_xp = statistics.mean(self.y_xp_arr)
+        self.y_yp = statistics.mean(self.y_yp_arr)
+        self.y_xp2 = statistics.mean(self.y_xp2_arr)
+        self.y_xpyp = statistics.mean(self.y_xpyp_arr)
+        self.y_yp2 = statistics.mean(self.y_yp2_arr)
+        self.y_xp3 = statistics.mean(self.y_xp3_arr)
+        self.y_xp2yp = statistics.mean(self.y_xp2yp_arr)
+        self.y_xpyp2 = statistics.mean(self.y_xpyp2_arr)
+        self.y_yp3 = statistics.mean(self.y_yp3_arr)
+
+        if(len(self.N_stars) > 1):
+            self.x_const_err = statistics.stdev(self.x_const_arr)
+            self.x_xp_err = statistics.stdev(self.x_xp_arr)
+            self.x_yp_err = statistics.stdev(self.x_yp_arr)
+            self.x_xp2_err = statistics.stdev(self.x_xp2_arr)
+            self.x_xpyp_err = statistics.stdev(self.x_xpyp_arr)
+            self.x_yp2_err = statistics.stdev(self.x_yp2_arr)
+            self.x_xp3_err = statistics.stdev(self.x_xp3_arr)
+            self.x_xp2yp_err = statistics.stdev(self.x_xp2yp_arr)
+            self.x_xpyp2_err = statistics.stdev(self.x_xpyp2_arr)
+            self.x_yp3_err = statistics.stdev(self.x_yp3_arr)
+
+            self.y_const_err = statistics.stdev(self.y_const_arr)
+            self.y_xp_err = statistics.stdev(self.y_xp_arr)
+            self.y_yp_err = statistics.stdev(self.y_yp_arr)
+            self.y_xp2_err = statistics.stdev(self.y_xp2_arr)
+            self.y_xpyp_err = statistics.stdev(self.y_xpyp_arr)
+            self.y_yp2_err = statistics.stdev(self.y_yp2_arr)
+            self.y_xp3_err = statistics.stdev(self.y_xp3_arr)
+            self.y_xp2yp_err = statistics.stdev(self.y_xp2yp_arr)
+            self.y_xpyp2_err = statistics.stdev(self.y_xpyp2_arr)
+            self.y_yp3_err = statistics.stdev(self.y_yp3_arr)
+
+    def printResults(self,fname,desc='',append=False):
+        openType = 'a' if append else 'w'
+        with open(fname,openType) as f:
+            if(not append):
+                f.write('desc,x_const,x_const_err,x_xp,x_xp_err,x_yp,x_yp_err,x_xp2,x_xp2_err,x_xpyp,x_xpyp_err,x_yp2,x_yp2_err,x_xp3,x_xp3_err,x_xp2yp,x_xp2yp_err,x_xpyp2,x_xpyp2_err,x_yp3,x_yp3_err,y_const,y_const_err,y_xp,y_xp_err,y_yp,y_yp_err,y_xp2,y_xp2_err,y_xpyp,y_xpyp_err,y_yp2,y_yp2_err,y_xp3,y_xp3_err,y_xp2yp,y_xp2yp_err,y_xpyp2,y_xpyp2_err,y_yp3,y_yp3_err\n')
+            f.write('%s,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e,%13.6e\n'%(desc,self.x_const,self.x_const_err,self.x_xp,self.x_xp_err,self.x_yp,self.x_yp_err,self.x_xp2,self.x_xp2_err,self.x_xpyp,self.x_xpyp_err,self.x_yp2,self.x_yp2_err,self.x_xp3,self.x_xp3_err,self.x_xp2yp,self.x_xp2yp_err,self.x_xpyp2,self.x_xpyp2_err,self.x_yp3,self.x_yp3_err,self.y_const,self.y_const_err,self.y_xp,self.y_xp_err,self.y_yp,self.y_yp_err,self.y_xp2,self.y_xp2_err,self.y_xpyp,self.y_xpyp_err,self.y_yp2,self.y_yp2_err,self.y_xp3,self.y_xp3_err,self.y_xp2yp,self.y_xp2yp_err,self.y_xpyp2,self.y_xpyp2_err,self.y_yp3,self.y_yp3_err))
--- test.py
+++ test.py
@@ -1,4 +1,30 @@
 from distortion import Distortion
+import tkinter as tk
+from tkinter import filedialog
+from tkinter.messagebox import askyesno
+from os.path import basename
 
-f1 = Distortion('Bruns HIP 100453 ED127W.log')
-print(f1.N_stars,f1.x_const,f1.y_const)
+## create a fake main window
+root = tk.Tk()
+root.withdraw()
+
+## ask for the filenames
+fname = filedialog.askopenfilenames()
+
+combine = False
+if(len(fname)>1):
+    combine = askyesno(title='Combine?', message='Would you like to combine all results from all files?')
+
+## run each file
+for name in fname:
+    f1 = Distortion(name)
+    print(f1.N_fields,f1.x_const,f1.x_const_err,f1.y_const,f1.y_const_err)
+    append = True if len(f1.N_stars)>1 else False
+    f1.printResults('output.csv',desc=basename(name),append=append)
+
+if(combine):
+    for name in fname:
+        fcomb = Distortion(name,reset=False)
+        desc = desc+'+'+basename(name) if len(fcomb.N_stars)>1 else basename(name)
+        print(fcomb.N_fields,len(fcomb.N_stars),fcomb.x_const,fcomb.x_const_err,fcomb.y_const,fcomb.y_const_err)
+        fcomb.printResults('output.csv',desc=desc,append=True)
